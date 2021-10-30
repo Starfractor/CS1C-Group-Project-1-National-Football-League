202128fef84dfd34864fc799c06e85aa776a3c8d
jasonk0201 <92902932+jasonk0201@users.noreply.github.com>
2021-10-29 21:27:02 -0700

    Table added for capacity window

--- capacitywindow.cpp
+++ capacitywindow.cpp
@@ -1,13 +1,17 @@
 #include "capacitywindow.h"
 #include "ui_capacitywindow.h"
+#include <QTableWidget>
 
 CapacityWindow::CapacityWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::CapacityWindow)
 {
     ui->setupUi(this);
-    this->setFixedSize(400,300);
+    this->setFixedSize(1000,700);
     this->setWindowTitle("Stadium Capacity");
+
+    QTableWidgetItem *newItem = new QTableWidgetItem("Test");
+    ui->tableWidget->setItem(0, 0, newItem);
 }
 
 CapacityWindow::~CapacityWindow()
